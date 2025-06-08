7c746653d050acf546604da055e5b42717406de3
Maycon Palomo <mcn630@gmail.com>
2025-06-08 12:49:40 -0300

    Add new commands to branch dag

--- dbt_airflow_dag/dags/branch.py
+++ dbt_airflow_dag/dags/branch.py
@@ -10,18 +10,22 @@ def branch():
     @task.branch
     def b(val: int):
         if val == 1:
-            return "equal_1"
+            return ["equal_1", "run_if_1"]
         return "different_than_1"
     
     @task
     def equal_1(val: int):
         print(f"Equal to {val}")
+        
+    @task
+    def run_if_1(val: int):
+        print(f"Run if {val} is 1")
     
     @task
     def different_than_1(val: int):
         print(f"Different than 1: {val}")
     
     val = a()
-    b(val) >> [equal_1(val), different_than_1(val)]
+    b(val) >> [equal_1(val), different_than_1(val), run_if_1(val)]
     
 branch()
